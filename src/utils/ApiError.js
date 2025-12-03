2628a7a1be23daf3bfedc55356019f59f8f1920c
rahuluke <rahuluke559@gmail.in>
2025-12-03 16:06:45 +0530

    util code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message="something went wron",
+        errors = [],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.errors = errors
+        this.success = false
+
+        if(stack){
+            this.stack = statck
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+        
+    }
+}
+
+export {ApiError}

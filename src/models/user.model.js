5e4b2f8a72eba789afc017abb44cb76eaac678a2
rahuluke <rahuluke559@gmail.in>
2025-12-11 15:57:12 +0530

    add Resigter Controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,7 +51,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -68,7 +68,7 @@ userSchema.methods.generateAccessToken = function(){
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
@@ -79,7 +79,7 @@ userSchema.methods.generateAccessToken = function(){
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         }
     )
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
 
-// router.route('/register').post(registerUser)
-
-router.post('/register', (req,res) => {
-    res.send("working");
-})
+router.route('/register').post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    );
 
 
 
